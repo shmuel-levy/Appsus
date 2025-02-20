391ff807d4b0ed67db135185087af3ea91bc86a9
Shoham Shtiler <shtilershoham@gmail.com>
2025-02-20 20:49:07 +0200

    Clear console.log 'hello'

--- app.js
+++ app.js
@@ -2,5 +2,3 @@ import { RootCmp } from './RootCmp.jsx'
 const elContainer = document.getElementById('root')
 const root = ReactDOM.createRoot(elContainer)
 root.render(<RootCmp />)
-
-console.log('hello')
