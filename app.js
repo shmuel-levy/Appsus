4060dcd22e09971b49ed0852a0e8fd87059ee8d9
Shoham Shtiler <shtilershoham@gmail.com>
2025-02-20 20:46:55 +0200

    Connect clone

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ import { RootCmp } from './RootCmp.jsx'
 const elContainer = document.getElementById('root')
 const root = ReactDOM.createRoot(elContainer)
 root.render(<RootCmp />)
+
+console.log('hello')
