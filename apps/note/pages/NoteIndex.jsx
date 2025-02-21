a70b48540b29e4b7043b68d771505045e0cf2d99
Shmuel levy <shmuellevy16@gmail.com>
2025-02-21 12:06:48 +0200

    uploaded the demo data weve got from the pdf file

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,4 +1,4 @@
-
+import {useState, useEffect} from 'react'
 export function NoteIndex() {
     return <section className="container">Notes app</section>
 }
--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,48 @@
 // note service
+import { storageService } from "../../../services/async-storage.service"
+import { utilService } from "../../../services/util.service"
+import { showSuccessMsg, showErrorMsg } from "../../../services/event-bus.service"
+const STORAGE_KEY = 'noteDB'
+
+const defaultNotes = [
+    {
+        id: 'n101',
+        createdAt: 1112222,
+        type: 'NoteTxt',
+        isPinned: true,
+        style: {
+            backgroundColor: '#00d'
+        },
+        info: {
+            txt: 'Fullstack Me Baby!'
+        }
+    },
+    {
+        id: 'n102',
+        createdAt: 1112223,
+        type: 'NoteImg',
+        isPinned: false,
+        info: {
+            url: 'http://some-img/me',
+            title: 'Bobi and Me'
+        },
+        style: {
+            backgroundColor: '#00d'
+        }
+    },
+    {
+        id: 'n103',
+        createdAt: 1112224,
+        type: 'NoteTodos',
+        isPinned: false,
+        info: {
+            title: 'Get my stuff together',
+            todos: [
+                { txt: 'Driving license', doneAt: null },
+                { txt: 'Coding power', doneAt: 187111111 }
+            ]
+        }
+    }
+]
+
+
