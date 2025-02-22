f06ab4a9b1b6dbc7571099a30addd23b1e753aad
Shoham Shtiler <shtilershoham@gmail.com>
2025-02-22 17:07:24 +0200

    Add basic model of mails to service

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,21 @@
 // mail service
+
+// • Model - start with a basic model of mails:
+
+const mail = {
+id: 'e101',
+createdAt : 1551133930500,
+subject: 'Miss you!',
+body: 'Would love to catch up sometimes',
+isRead: false,
+sentAt : 1551133930594,
+removedAt : null,
+from: 'momo@momo.com',
+to: 'user@appsus.com'
+}
+
+const loggedinUser = {
+    email: 'user@appsus.com',
+    fullname: 'Mahatma Appsus'
+   }
+   
