d432e98331e83c833a197a543e854e1d40b4f89f
shmuel-levy <shmuellevy16@gmail.com>
2025-02-25 14:54:59 +0200

    Showing notes list

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,3 +1,18 @@
-export function NoteList() {
-    return <div>note list</div>
+import { NotePreview } from './NotePreview.jsx'
+
+
+export function NoteList({notes, onRemoveNote, onPinNote}) {
+    if (!notes.length) return <div>No notes to display</div>
+    return (
+        <div className = 'notes-list'>
+            {notes.map(note=>(
+                <NotePreview
+                key={note.id}
+                note = {note}
+                onRemoveNote = {onRemoveNote}
+                onPinNote = {onPinNote} 
+                />
+            ))}
+        </div>
+    )
 }
