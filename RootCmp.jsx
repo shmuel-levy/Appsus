c13b5dc6f5b428163f712301894a6f1efc6b836c
shmuel-levy <shmuellevy16@gmail.com>
2025-03-06 12:46:02 +0200

    Change images in About page

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,41 +1,46 @@
 const { Route, Routes } = ReactRouterDOM
 const Router = ReactRouterDOM.HashRouter
 
-import { AppHeader } from './cmps/AppHeader.jsx'
-import { UserMsg } from './cmps/UserMsg.jsx'
-import { About } from './pages/About.jsx'
-import { Home } from './pages/Home.jsx'
+import { AppHeader } from "./cmps/AppHeader.jsx"
+import { UserMsg } from "./cmps/UserMsg.jsx"
+import { About } from "./pages/About.jsx"
+import { Home } from "./pages/Home.jsx"
 
-import { MailIndex } from './apps/mail/pages/MailIndex.jsx'
-import { MailDetails } from './apps/mail/pages/MailDetails.jsx'
-import { MailCompose } from './apps/mail/cmps/MailCompose.jsx'
+import { MailIndex } from "./apps/mail/pages/MailIndex.jsx"
+import { MailDetails } from "./apps/mail/pages/MailDetails.jsx"
+import { MailCompose } from "./apps/mail/cmps/MailCompose.jsx"
 
-import { NoteApp } from './apps/note/pages/NoteApp.jsx'
-import { NoteIndex } from './apps/note/pages/NoteIndex.jsx'
-import { NoteEdit } from './apps/note/pages/NoteEdit.jsx'
+import { NoteApp } from "./apps/note/pages/NoteApp.jsx"
+import { NoteIndex } from "./apps/note/pages/NoteIndex.jsx"
+import { NoteEdit } from "./apps/note/pages/NoteEdit.jsx"
 
 export function RootCmp() {
-    return <Router>
-        <section className="root-cmp">
-            <AppHeader />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
+  return (
+    <Router>
+      <section className="root-cmp">
+        <AppHeader />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
 
-                <Route path="/mail" element={<MailIndex />} >
-                    <Route path="/mail/:mailId" element={<MailDetails />} />
-                </Route>
-                <Route path="/mail/compose" element={<MailCompose />} />
-                
-                <Route path="/note" element={<NoteApp />}>
-                    <Route index element={<NoteIndex />} />
-                    <Route path=":noteId" element={<NoteEdit />} />
-                    <Route path="reminders" element={<div className="coming-soon"> </div>} />
-                    <Route path="archive" element={<NoteIndex isArchive={true} />} />
-                    <Route path="trash" element={<NoteIndex isTrash={true} />} />
-                </Route>
-            </Routes>
-            <UserMsg />
-        </section>
+          <Route path="/mail" element={<MailIndex />}>
+            <Route path="/mail/:mailId" element={<MailDetails />} />
+          </Route>
+          <Route path="/mail/compose" element={<MailCompose />} />
+
+          <Route path="/note" element={<NoteApp />}>
+            <Route index element={<NoteIndex />} />
+            <Route path=":noteId" element={<NoteEdit />} />
+            <Route
+              path="reminders"
+              element={<div className="coming-soon"> </div>}
+            />
+            <Route path="archive" element={<NoteIndex isArchive={true} />} />
+            <Route path="trash" element={<NoteIndex isTrash={true} />} />
+          </Route>
+        </Routes>
+        <UserMsg />
+      </section>
     </Router>
+  )
 }
--- pages/About.jsx
+++ pages/About.jsx
@@ -1,61 +1,84 @@
 const { useState } = React
 
 export function About() {
-    return (
-        <div className="about-page">
-            <div className="about-container">
-                <h1>About Appsus</h1>
-                
-                <section className="about-app">
-                    <h2>Our Application</h2>
-                    <p>
-                        Appsus is a productivity suite inspired by Google's applications. It features a notes app similar to 
-                        Google Keep and an email client similar to Gmail. Our goal is to provide a seamless experience 
-                        for managing your notes and communications.
-                    </p>
-                </section>
-                
-                <section className="team-section">
-                    <h2>Meet The Team</h2>
-                    <div className="team-members">
-                        <div className="team-member">
-                            <img src="assets/img/1.png" alt="Team Member" className="member-img" />
-                            <h3>Shmuel Levy</h3>
-                            <p>Front-end Developer</p>
-                            <p>Responsible for the Keep application and About page.</p>
-                            <div className="social-links">
-                                <a href="https://github.com/shmuel-levy?tab=repositories" target="_blank">GitHub</a>
-                                <a href="https://linkedin.com/" target="_blank">LinkedIn</a>
-                            </div>
-                        </div>
-                        
-                        <div className="team-member">
-                            <img src="assets/img/2.png" alt="Team Member" className="member-img" />
-                            <h3>Shoham Shitler</h3>
-                            <p>Front-end Developer</p>
-                            <p>Responsible for the Mail application and Home page.</p>
-                            <div className="social-links">
-                                <a href="https://github.com/" target="_blank">GitHub</a>
-                                <a href="https://linkedin.com/" target="_blank">LinkedIn</a>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                
-                <section className="technologies">
-                    <h2>Technologies Used</h2>
-                    <ul className="tech-list">
-                        <li>React</li>
-                        <li>React Router</li>
-                        <li>CSS</li>
-                        <li>Javascript</li>
-                    </ul>
-                </section>
-                
-                <footer className="about-footer">
-                    <p>© 2025 Appsus | Made by Shmuel and Shoham</p>
-                </footer>
+  return (
+    <div className="about-page">
+      <div className="about-container">
+        <h1>About Appsus</h1>
+
+        <section className="about-app">
+          <h2>Our Application</h2>
+          <p>
+            Appsus is a productivity suite inspired by Google's applications. It
+            features a notes app similar to Google Keep and an email client
+            similar to Gmail. Our goal is to provide a seamless experience for
+            managing your notes and communications.
+          </p>
+        </section>
+
+        <section className="team-section">
+          <h2>Meet The Team</h2>
+          <div className="team-members">
+            <div className="team-member">
+              <img
+                src="assets/img/Shmuel.jpg"
+                alt="Team Member"
+                className="member-img"
+              />
+              <h3>Shmuel Levy</h3>
+              <p>Front-end Developer</p>
+              <p>Responsible for the Keep application and About page.</p>
+              <div className="social-links">
+                <a
+                  href="https://github.com/shmuel-levy?tab=repositories"
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+                <a href="https://linkedin.com/" target="_blank">
+                  LinkedIn
+                </a>
+              </div>
             </div>
-        </div>
-    )
+
+            <div className="team-member">
+              <img
+                src="assets/img/SHOHAM PICTURE.jpg"
+                alt="Team Member"
+                className="member-img"
+              />
+              <h3>Shoham Shitler</h3>
+              <p>Front-end Developer</p>
+              <p>Responsible for the Mail application and Home page.</p>
+              <div className="social-links">
+                <a href="https://github.com/" target="_blank">
+                  GitHub
+                </a>
+                <a href="https://linkedin.com/" target="_blank">
+                  LinkedIn
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="technologies">
+          <h2>Technologies Used</h2>
+          <ul className="tech-list">
+            <li>React</li>
+            <li>React Router</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>Lottie</li>
+            <li>SVG</li>
+
+          </ul>
+        </section>
+
+        <footer className="about-footer">
+          <p>© 2025 Appsus | Made by Shmuel and Shoham</p>
+        </footer>
+      </div>
+    </div>
+  )
 }
