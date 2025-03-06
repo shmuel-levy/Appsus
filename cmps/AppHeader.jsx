88452da858e82532331c42e997f9a5345fbe8cf9
Shoham Shtiler <shtilershoham@gmail.com>
2025-03-06 16:48:22 +0200

    Add app-header style

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -3,13 +3,21 @@ const { Link, NavLink } = ReactRouterDOM
 export function AppHeader() {
     return <header className="app-header">
         <Link to="/" className="logo-text">
-            APPSUS
+            <img src='./assets/img/horse.png' className='header-logo'></img>
         </Link>
         <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
+                <NavLink to="/" className={({ isActive }) => isActive ? "active" : "home-link"}>
+                    <i className="fa-solid fa-house"></i> Home
+                </NavLink>
+                <NavLink to="/about" className={({ isActive }) => isActive ? "active" : "about-link"}>
+                    <i className="fa-solid fa-circle-info"></i> About
+                </NavLink>
+                <NavLink to="/mail" className={({ isActive }) => isActive ? "active" : "mail-link"}>
+                    <i className="fa-solid fa-envelope"></i> Mail
+                </NavLink>
+                <NavLink to="/note" className={({ isActive }) => isActive ? "active" : "note-link"}>
+                    <i className="fa-solid fa-lightbulb"></i> Note
+                </NavLink>
+            </nav>
     </header>
 }
